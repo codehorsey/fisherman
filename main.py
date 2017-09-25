c904da0a3bb8f038e902570567e2f451028def75
codehorsey <codehorsey@gmail.com>
2017-09-25 15:13:14 -0500

    added logger and prettifying prints

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from numpy.random import choice 
 import random
+import sys
+import time
 from collections import Counter
 from readtext import fishie_value_list as fishvalues, fish_and_locations as fishcations 
+import logging
+logging.basicConfig(level=50)
+logger = logging.getLogger(__name__)
+logger.setLevel(10)
+# use diable to toggle debug mode
+logging.disable(50)
 
 class GameState():
 
@@ -29,14 +37,25 @@ class Fisherman():
 		self.nothing_or_fish_weights = [.6, .4]
 
 	def fish(self):
+		fish = ''
+		print "-" * 42
+		print "Fisherman Casts..."
+		ones_place = random.randrange(5)
+		tenth_place = random.randrange(0,10)
+		time_next_cast = str(ones_place) + '.' + str(tenth_place)
+		time_next_cast = float(time_next_cast)
+		logger.debug('Next cast in: {}'.format(time_next_cast/10))
+		time.sleep(time_next_cast/10)
 		fish_or_not = choice(self.nothing_or_fish, p=self.nothing_or_fish_weights)
 		if fish_or_not == 'nothing':
+			print "Nothing!"
 			return fish_or_not
 		else:
-			# Hooked a fish, but what kind!?
-			# Based on location - give different types of fish
-			# return 'redfish'
-			return self.select_fish()
+			fish = self.select_fish()
+			print "Caught a {}".format(fish) 
+			return fish
+
+
 
 
 	def select_fish(self):
@@ -66,13 +85,16 @@ def day_cycle(game, fisherman):
 	game.day += 1
 	tweather = choice(game.weather, p=game.weather_weights)
 
+	print "-" * 42
 	print "Todays weather is {}.".format(tweather)
 	print "Today is Day {}".format(game.day)
+	print "Location: {}".format(man.location)
+	
 	if tweather == 'rain':
 		fisherman.nothing_or_fish_weights = modify_weights(fisherman.nothing_or_fish_weights, [.3, -.3])
 	
 	# man fishes 200 times and appends results to l
-	l = [man.fish() for _ in range(200)]
+	l = [man.fish() for _ in range(10)]
 	day_results = l
 	day = 'Day {}'.format(game.day)
 	man.history[day] = day_results
@@ -83,18 +105,25 @@ def day_cycle(game, fisherman):
 	man.inventory.append(filtered_l)
 	man.inventory2.extend(filtered_l)
 
-	print "Todays Summary: {}".format(Counter(l))
+	print "*" * 42
+	logger.debug("Todays Summary: {}".format(Counter(l)))
+	for k, v in (Counter(l).items()):
+		if k == 'nothing':
+			continue
+		else:
+			print k, v
+	print "*" * 42
 
 	# answer = raw_input('Do you want to sell your fish?')
 	answer = 'n'
-	man.travel('River')
-	print man.location
+	man.travel(random.choice(man.locations.keys()))
 	if answer.lower() != 'n':
 		for fish in man.inventory2[:]:
 			man.money += man.sell_fish(fish)
 			man.inventory2.remove(fish)
 			print 'Money: {}'.format(man.money)
 
+	print "_" * 42
 
 
 def modify_weights(weights, modified_weights):
@@ -111,18 +140,7 @@ game = GameState()
 while True:
 
 	day_cycle(game, man)
-	if game.day == 10:
+	if game.day == 5:
 		break
-
-
-
-
-
-
-
-# print Counter(sum(man.inventory, []))
-# print man.inventory2
-# print len(man.inventory2)
-# print Counter(man.inventory2)
-
-print len(man.history['Day 1'])
+	raw_input('enter')
+	print(chr(27) + "[2J")
